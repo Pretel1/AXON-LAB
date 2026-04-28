f9b68729f98bcea9802cc5e29906971bbcc64490
Pretel1 <145733650+Pretel1@users.noreply.github.com>
2026-04-28 12:01:29 -0500

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -5,6 +5,9 @@ const SUPABASE_URL = 'https://xxrxoyotgragaunygsne.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_dmHdG4_xal2iWHFagZjo2Q_PFtxHfPJ';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Buckets
 export const LABS_BUCKET = 'laboratorios';
+export const MEDIA_BUCKET = 'media';
 
 console.log('✅ Supabase configurado correctamente');
